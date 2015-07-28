152238f9579475d7b84d1c411311e5d5d697ab64
Karthikeyan S <keya@lostinkaos.com>
2015-07-28 18:37:16 +0530

    Browsing the Gallery

--- NoteSquirrel/app/src/main/java/com/lostinkaos/android/notesquirrel/MainActivity.java
+++ NoteSquirrel/app/src/main/java/com/lostinkaos/android/notesquirrel/MainActivity.java
@@ -1,9 +1,13 @@
 package com.lostinkaos.android.notesquirrel;
 
+import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
@@ -15,6 +19,7 @@ import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.DataInputStream;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.InputStreamReader;
@@ -26,6 +31,10 @@ public class MainActivity extends ActionBarActivity {
     public static final String TEXTFILE = "notesquirrel2.txt";
     public static final String FILESAVED = "FileSaved";
     public static final String RESET_PASSPOINTS = "ResetPasspoints";
+
+    private File image;
+    private static final int PHOTO_TAKEN_REQUEST = 0;
+    private static final int BROWSE_GALLERY_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +49,74 @@ public class MainActivity extends ActionBarActivity {
             loadSavedFile();
         }
     }
+
+    private void replaceImage() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        View v = getLayoutInflater().inflate(R.layout.replace_image, null);
+        builder.setTitle(R.string.replace_lock_image);
+        builder.setView(v);
+
+        final AlertDialog dlg = builder.create();
+        dlg.show();
+
+        final Button takePhoto = (Button) v.findViewById(R.id.take_photo);
+        final Button browseGallery = (Button) v.findViewById(R.id.browse_gallery);
+
+        takePhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                takePhoto();
+            }
+        });
+
+        browseGallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                browseGallery();
+            }
+        });
+
+    }
+
+    private void browseGallery() {
+        Intent i = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(i, BROWSE_GALLERY_REQUEST);
+    }
+
+    private void takePhoto() {
+        File picturesDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        image = new File(picturesDirectory, "passpoints_image");
+
+        Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        i.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(image));
+        startActivityForResult(i, PHOTO_TAKEN_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if( requestCode == BROWSE_GALLERY_REQUEST ) {
+            Toast.makeText(this, "Gallery result: " + data.getData(), Toast.LENGTH_LONG ).show();
+        }
+
+        if( requestCode == PHOTO_TAKEN_REQUEST ) {
+            if( image == null ) {
+                Toast.makeText(this, "Unable to display image", Toast.LENGTH_LONG).show();
+                Log.d(DEBUGTAG, "Photo: " + image.getPath());
+            }
+        }
+
+        resetPasspoints();
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void resetPasspoints() {
+        Intent i = new Intent(MainActivity.this, ImageActivity.class);
+        i.putExtra(RESET_PASSPOINTS, true);
+        startActivity(i);
+    }
+
 
     private void loadSavedFile() {
         try {
@@ -116,9 +193,10 @@ public class MainActivity extends ActionBarActivity {
         //noinspection SimplifiableIfStatement
         switch (id) {
             case R.id.menu_passpoints_reset:
-                Intent i = new Intent(MainActivity.this, ImageActivity.class);
-                i.putExtra(RESET_PASSPOINTS, true);
-                startActivity(i);
+                resetPasspoints();
+                return true;
+            case R.id.menu_replace_image:
+                replaceImage();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
